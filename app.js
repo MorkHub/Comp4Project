e8f77b52df0bb442031d3677782b84c795caaaca
Mark Cockburn <Mork@TheMork.co.uk>
2015-12-07 00:58:16 +0000

    Basic login system functional

--- app.js
+++ app.js
@@ -1,28 +1,78 @@
-var express = require("express");
-var app = express();
-var io = require("socket.io");
-var ejs = require("ejs");
-var port = 8080 || process.env.port;
+// Inport modules
+var io         = require ( "socket.io" );
+var ejs        = require ( "ejs" );
+var express    = require ( "express" );
+var session    = require ( "express-session" );
+var bodyParser = require ( "body-parser" );
+var app        = express ();
+var port       = 8080 || process.env.port;
 
-//app.use(express.static(__dirname + "/public"));
-console.log("hi")
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+// Init test users
+var users = [];
+users[0] = { name: "Mark Cockburn", email: "mark@localhost", password: "test", access: 10 }
+users[1] = { name: "Test Student", email: "test@localhost", password: "test", access: 1 }
 
-// index page 
-app.get('/', function(req, res) {
-    res.render('index');
+// Enable modules & init
+app.set ( 'view engine', 'ejs' );
+app.use ( bodyParser.json () );
+app.use ( bodyParser.urlencoded ( { extended: true } ) );
+app.use ( session ( { secret: "isaacnewton", resave: true, saveUninitialized: true } ) );
+
+// Routes
+app.get ( '/', function ( req, res )
+{
+  res.render ( 'index', { valid: req.session.valid, user: req.session.user } )
+});
+app.get ( '/profile', auth, function ( req, res )
+{
+  res.render ( 'profile', { valid: req.session.valid, user: req.session.user } );
+});
+app.get ( '/admin', auth, function ( req, res )
+{
+  if ( req.session.user.access >= 10 )
+  {
+    res.render ( 'admin', { valid: req.session.valid, user: req.session.user, users: users } );
+  } else {
+    res.redirect ( '/' );
+  }
+});
+app.post ( '/login', function ( req, res )
+{
+  var post = req.body; // Get POST data
+  req.session.valid = false; // Init validation
+  console.log (post) // debug
+  users.forEach ( function ( user, index ) // Iterate through valid users and check credentials
+  {
+    if ( ( user.email == post.email ) && ( user.password == post.passwd ) )
+    { req.session.valid = true; req.session.user = users[index]; console.log ( users[index].name + " logged in" ) } // store user data in session
+  });
+  if ( !req.session.valid )
+  {
+    res.redirect ( "/" ); // redirect to home if invalid
+  } else {
+    res.redirect ( "/profile" ); // redirect to profile if valid
+  }
+});
+// Logs out ( deletes session data )
+app.get ( '/logout', function ( req, res ) {
+  req.session.user = undefined;
+  req.session.valid = false;
+  res.redirect ( "/" );
 });
 
-// about page 
-app.get('/about', function(req, res) {
-    res.render('about');
-});
+// Verify session, if invalid display error
+function auth ( req, res, next )
+{
+  if ( !req.session.valid )
+  {
+    res.send ( "<h1>Error 401</h1> Please <a href=\"/\">go home</a> and sign in." );
+  } else {
+    next();
+  }
+}
 
-app.listen(port, function(){
-  console.log("Running on *:" + port);
+// Listen on defined port, and print debug
+app.listen ( port, function ()
+{
+  console.log ( "Running on *:" + port );
 })
-
-//io.on('connection', function(c){
-//  console.log(c);
-//})
