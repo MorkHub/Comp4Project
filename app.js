a4d4c2b38608c46bc464aba8130dbb2b0b731ec3
Mark Cockburn <mork@themork.co.uk>
2016-01-03 20:20:11 -0500

    minified & modularised main app code

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var port       = ( process.env.computername == "ANNE" ) ? ( 80 ) : ( 25564 || process.env.port );
-// Inport modules
-var io         = require ( "socket.io" )( app ),
-    session    = require ( "express-session" ),
-    bodyParser = require ( "body-parser" ),
-		data       = require ( "./defines.js" ),
-		express    = require ( "express" ),
-    flat       = require ( "flat.js" ),
-		ejs        = require ( "ejs" ),
-    app        = express ();
+// Import modules
+var session			= require ( "express-session" ),
+    bodyParser	= require ( "body-parser" ),
+		datatypes		= require ( "./defines.js" ),
+		express			= require ( "express" ),
+    flat				= require ( "flat.js" ),
+		ejs					= require ( "ejs" ),
+		io					= require ( "socket.io" )( 25566 ),
+		crypto			=	require ( "./crypto.js" ),
+    app					= express (),
+		host				= "http://themork.co.uk/",
+		port				= ( process.env.computername == "ANNE" ) ? ( 80 ) : ( 25564 || process.env.port ),
+		args 				= process.argv; args.shift(); args.shift();
 
 // database init
 var db = flat ( "store.db", { persist: true } );
-
-// Init tasks
-var tasks = [];
 
 // Enable modules & init
 app.set ( 'view engine', 'ejs' ); // view engine; used to render pages, with data passed to html
@@ -21,22 +21,8 @@ app.use ( bodyParser.json () ); //  bodyParser allows me to receive data from po
 app.use ( bodyParser.urlencoded ( { extended: true } ) ); // additional config for ^
 app.use ( session ( { secret: "isaacnewton", resave: true, saveUninitialized: true } ) ); // manipulate signed session data
 
-// Routes
+// Routes & compartmentalisation
 app.use ( express.static ( __dirname + '/public' ) ); // serve any content from public/
-require ( './routes.js' )( express, app, db, ejs );
-
-// client-server communication
-io.on('connection', function (socket)
-{
-	socket.emit ( 'request', { data: "hello" });
-	socket.on ( 'response', function ( data ) {
-		console.log ( "data:", data.data );
-		console.log ( "user:", data.user );
-	});
-});
-
-// Listen on defined port, and print debug
-app.listen ( port, function ()
-{
-	console.log ( "Running on *:" + port );
-})
+require ( './routes.js' )( express, app, db, ejs, datatypes, crypto );
+require ( './io.js' )( app, db, io, host, crypto );
+app.listen ( port, function () { console.log ( "Running on *:" + port ) });
