90d3ff7960fab13388284073b6ee6e8d2a403ee9
Mark Cockburn <mork@themork.co.uk>
2016-01-03 20:13:41 -0500

    updated definitions to create directories for new users, and remove when deleting

--- defines.js
+++ defines.js
@@ -1,28 +1,59 @@
-var md5 = require ( "md5" );
+var md5		= require ( "md5" );
+var exec	= require ( "child_process" ).exec;
+var dir 	= "/home/mork/project/public/media/schools"
+function mkdir ( user, school )
+{
+	if ( school !== undefined )
+	{
+		exec ( "mkdir " + dir + "/" + school + "/users/" + user + "; cp " + dir + "/default/* " + dir + "/" + school + "/users/" + user + "/", function ( error, stdout, stderr ) { if ( error ) { console.log ( "ERROR: " + error ) } } );
+	} else {
+		console.log ( "ERROR: Parameters missing ( usage: mkdir ( username, school )" );
+	}
+}
+function rmdir ( user, school )
+{
+	if ( school !== undefined )
+	{
+		exec ( "rm " + dir + "/" + school + "/users/" + user + "/* -r; rmdir " + dir + "/" + school + "/users/" + user, function ( error, stdout, stderr ) { if ( error ) { console.log ( "ERROR: " + error ) } } )
+	} else {
+		console.log ( "ERROR: Parameters missing ( usage: rmdir ( username, school )" );
+	}
+}
+
 module.exports = {
-  User : function ( name, username, email, password, school, access, avatar, teacher )
+  User : function ( name, username, password, school, access, teacher, avatar )
   {
     this.id;
     this.name = name || "John Smith";
-    this.email = email || "jsmith@mail.com";
+    // this.email = email || "jsmith@mail.com";
     this.username = username;
     this.password = password || "password123";
     this.school = school || "";
     this.access = parseInt ( access ) || 1;
-    if ( avatar ) { this.avatar = avatar.replace (/ /g, "%20" ) } else { this.avatar = "/media/schools/" + this.school + "/users/" + this.username + "/profile.png" };
+    if ( avatar )
+		{
+			this.avatar = avatar.replace (/ /g, "%20" ) } else { this.avatar = "/media/schools/" + this.school + "/users/" + this.username + "/avatar.png"
+			mkdir ( username, school )
+		};
     this.id = md5 (
       this.name.replace ( / /g, "" ) +
       this.email +
       this.password +
       this.school +
-      this.access +
-      this.avatar
+      this.access
     );
     this.tasksDone = [];
     this.score = this.name.length * 6;
     this.grade = grade ( this.score, 100 );
-    this.teacher = teacher;
-  }.bind(this)
+    this.teacher = teacher || "rsanchez14";
+  }.bind(this),
+	RemoveUser : function ( username, school )
+	{
+		if ( school !== undefined )
+		{
+			rmdir ( username, school )
+		}
+	}
 }
 
 function grade ( score, total )
