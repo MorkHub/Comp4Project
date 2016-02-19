bcec27bab4391650fb9fade5dd8ad1fca0a65d31
Mark Cockburn <mork@themork.co.uk>
2016-02-19 15:09:24 -0500

    Added error handling

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ var session    = require ( "express-session" ),
 	port       = ( process.env.computername == "ANNE" ) ? ( 80 ) : ( 25564 || process.env.port ),
 	args       = process.argv; args.shift(); args.shift();
 
+function errorHandler ( err, req, res, next ){
+	var user = db.getField( "users", req.session.user_id );
+	var status = req.session.status; req.session.status = undefined;
+	res.status(500);
+	res.render ( 'error', {
+		user_id: req.session.user_id,
+		user: user,
+		status: status,
+		error: err
+	});
+}
+
 // database init
 var db = flat ( "store.db", { persist: true } );
 
@@ -25,4 +37,5 @@ app.use ( session ( { secret: "isaacnewton", resave: true, saveUninitialized: tr
 app.use ( express.static ( __dirname + '/public' ) ); // serve any content from public/
 require ( './routes.js' )( express, app, db, ejs, datatypes, crypto );
 require ( './io.js' )( app, db, io, host, crypto );
+app.use ( errorHandler );
 app.listen ( port, function () { console.log ( "Running on *:" + port ) });
